8949c1285914f5e5e8c882eb78b9e111869a6540
Elena Fernandez-Victorio <elenafervic@gmail.com>
2024-08-13 14:26:11 +0100

    Started working on trying to code the neural network example in chapte 2 of the book. Have some issues with importing the necessary modules, but it seems to work.

--- NeuralNetwork_Chapter2.py
+++ NeuralNetwork_Chapter2.py
@@ -1,2 +1,8 @@
-from tensorflow.keras.datasets import  mnist
+from tensorflow.keras.datasets import mnist
+import matplotlib.pyplot as plt
+
 (train_images,train_labels), (test_images,test_labels)=mnist.load_data()
+print(train_labels[0])
+digit=train_images[0]
+plt.imshow(digit,cmap=plt.cm.binary)
+plt.show()
