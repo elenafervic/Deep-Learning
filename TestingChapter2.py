5f912ba8d42a28bc1a1690fa2fe34ae7a55a3ab8
Elena Fernandez-Victorio <elenafervic@gmail.com>
2024-09-12 17:51:59 +0100

    Some new testing

--- TestingChapter2.py
+++ TestingChapter2.py
@@ -1,10 +1,16 @@
-#import numpy
+import numpy
+import math
 #A=numpy.array([[[1,2],[3,4]],[[5,6],[7,8]]])
 #print(A.shape)
 #print(A)
 #A=A.reshape(8,1)
 #print(A)
 
-w=(2,3)
-print(w)
-print(type(w))
+w=[1,2]
+b=[3,4]
+a=w+b
+print(a)
+
+images=numpy.array([[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]]) #The length is 3
+print(len(images))
+print(math.ceil(len(images)))
