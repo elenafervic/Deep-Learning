d93c1481bb989c4d4eda9b2ee5beebc494460e83
Elena Fernandez-Victorio <elenafervic@gmail.com>
2024-09-13 14:22:34 +0100

    Ready to test-drive the neural network. I have reshaped the training images and fit the model.

--- ZenosAI.py
+++ ZenosAI.py
@@ -89,19 +89,20 @@ def fit(model,images,labels,epochs,batch_size=128):#We will run the training ste
                 print(f"loss at batch {batch_counter}:{loss:2f}")#Not sure exactly what this means
 
 
-
-
-
-
-
 #Can now make a model:
 model=NaiveSequential([
     NaiveDense(input_size=28*28,output_size=512,activation=tf.nn.relu),
     NaiveDense(input_size=512,output_size=10,activation=tf.nn.softmax)    
     ]) #We create a Naive sequential object called model with two deep layers.
-assert len(model.weights)==4
 
-#print(model.weights)
+(train_images,train_labels), (test_images,test_labels)=mnist.load_data()
+train_images=train_images.reshape((60000,28*28))
+train_images=train_images.astype("float32")/255
+test_images=test_images.reshape((10000,28*28))
+test_images=test_images.astype("float32")/255
+
+fit(model,train_images,train_labels,epochs=10,batch_size=128)
+
 
 
 
