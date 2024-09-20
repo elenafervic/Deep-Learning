da85bf642c65ec4fee9b63fa810cd707bf7b5c5a
Elena Fernandez-Victorio <elenafervic@gmail.com>
2024-09-20 18:07:00 +0100

    Some more testing of the outputs of time_series_dataset_from_array()

--- TestingChapter10.py
+++ TestingChapter10.py
@@ -4,15 +4,22 @@ from tensorflow import keras
 Numbers=[0,1,2,3,4,5,6,7,8,9]#This represents the training data set.
 
 dummy_dataset = keras.utils.timeseries_dataset_from_array(
-    data=Numbers[0:-3],#From begginign to the -3th term, not including the -3 term.
+    data=Numbers[0:-3],#From begginign to the -3th term, not including the -3 term. Not generally good, I usually want it to use all the dataset.
     targets=Numbers[3:],
     sequence_length=3,
     batch_size=2,
 )
-print(dummy_dataset)
 
 
-for inputs, targets in dummy_dataset: 
-    print(inputs, targets)
-    for i in range(inputs.shape[0]):
-        print([int(x) for x in inputs[i]], int(targets[i]))
+
+a,b,c= dummy_dataset
+print("a",a)
+print("b",b)
+print("c",c)
+print("Stop")
+for inputs, targets in dummy_dataset:#This loops through the batches. Inputs holds the current batch
+    print(inputs)
+    print(targets)
+    for i in range(inputs.shape[0]):#Loops through all the time series. inputs.shape[0] is the number of time series within that batch (most batches have same size, but last batch may have less).
+        print(i)
+        print([int(x) for x in inputs[i]], int(targets[i]))#It loops through all the values in a specific time series
